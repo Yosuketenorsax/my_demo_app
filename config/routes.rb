f8c83df0040766f90f14bc1a56b43ef1c1fcd945
YosukeSakamura <yosuketenor@gmail.com>
2015-10-12 16:24:30 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # get 'books/index'
+
+  # get 'books/show'
+  resources :books
+  root 'books#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
